cc2dcecebf126fd40c076c916c4ae9106a62a270
mabubakarsiddiq11 <mabubakarsiddiq11@gmail.com>
2025-06-11 13:49:03 +0500

    Add This Filter Option

--- src/Components/BasicTable.jsx
+++ src/Components/BasicTable.jsx
@@ -3,6 +3,7 @@ import {
   flexRender,
   getCoreRowModel,
   useReactTable,
+  getFilteredRowModel,
 } from "@tanstack/react-table";
 import mockData from "./MockData.json";
 import { columns } from "./Columns";
@@ -11,17 +12,31 @@ import React, { useState } from "react";
 function BasicTable() {
   const finalData = React.useMemo(() => mockData, []);
   const FinalColumns = React.useMemo(() => columns, []);
+  const [filtering, setFiltering] = useState("");
 
   const tableInstance = useReactTable({
     data: finalData,
     columns: FinalColumns,
     getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onGlobalFilterChange: setFiltering,
+    state: {
+      globalFilter: filtering,
+    },
   });
 
   // console.log("Test", tableInstance.getHeaderGroups());
 
   return (
     <div>
+       <input
+        type="text"
+        placeholder="Search..."
+        value={filtering}
+        onChange={(e) => setFiltering(e.target.value)}
+      />
+      <br />
+      <hr />
       <table>
         <thead>
           {tableInstance.getHeaderGroups().map((headerGroup) => (
